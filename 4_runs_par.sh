dd134f9a15d4483ae552f7f43f2f9d80ade4478a
vampchen <62760379+vampchen@users.noreply.github.com>
2023-09-20 15:51:54 -0400

    Update 4_runs_par.sh

--- 4_runs_par.sh
+++ 4_runs_par.sh
@@ -7,16 +7,12 @@ run_in_new_terminal() {
 
 # List of folder paths
 folders=(
-  "/media/qiang/B8707D67707D2D6E/CodesGoodNewark/WMCOG/2026_2045_adp"
-  "/media/qiang/B8707D67707D2D6E/CodesGoodNewark/WMCOG/2026_2045_rep"
-  "/media/qiang/B8707D67707D2D6E/CodesGoodNewark/WMCOG/2026_2045_ncc"
+  "/media/WMCOG/"
   # Add more folders here if needed
 )
 
 # List of corresponding commands for each folder
 commands=(
-  "./tap 0.0001 1 net_2030.txt sim_ctrl_2030.txt net_2040.txt sim_ctrl_2040.txt event_series_ok.txt rut_depth_ok.txt trip_mat_2030.csv trip_mat_2040.csv"
-  "./tap 0.0001 1 net_2030.txt sim_ctrl_2030.txt net_2040.txt sim_ctrl_2040.txt event_series_ok.txt rut_depth_ok.txt trip_mat_2030.csv trip_mat_2040.csv"
   "./tap 0.0001 1 net_2030.txt sim_ctrl_2030.txt net_2040.txt sim_ctrl_2040.txt event_series_ok.txt rut_depth_ok.txt trip_mat_2030.csv trip_mat_2040.csv"
   # Add more commands here if needed
 )
